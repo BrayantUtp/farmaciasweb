53ebbd7869c0ab8ecdb1c5dd3fe1a1396da88ce3
Alberto Toscano <a.toscano.tello@gmail.com>
2025-11-21 03:12:49 -0500

    Implementacion Home y mejora en pedidos

--- src/main/java/pe/edu/utp/boticaapp/service/impl/PedidoServiceImpl.java
+++ src/main/java/pe/edu/utp/boticaapp/service/impl/PedidoServiceImpl.java
@@ -5,8 +5,13 @@ import pe.edu.utp.boticaapp.repository.PedidoRepository;
 import pe.edu.utp.boticaapp.entity.*;
 import pe.edu.utp.boticaapp.service.PedidoService;
 import java.util.List;
+import java.util.Collections;
 @Service @RequiredArgsConstructor
 public class PedidoServiceImpl implements PedidoService {
   private final PedidoRepository repo;
-  @Override public List<Pedido> listar(Usuario u){ return repo.findByUsuarioOrderByCreadoEnDesc(u); }
+  @Override public List<Pedido> listar(Usuario u){ 
+    List<Pedido> lista = repo.findByUsuarioOrderByCreadoEnDesc(u);
+    Collections.reverse(lista);
+    return lista;
+  }
 }
